ecbe2a2817a1f115370807891e726d42241031d4
Dyslexus <96348686+Dragonpro9827@users.noreply.github.com>
2022-10-13 16:18:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ c=database.cursor()
 c.execute("DROP TABLE ULB")
 database.commit()
 c.execute('''CREATE TABLE IF NOT EXISTS ulb
-             (time json PRIMARY KEY,
+             (time json,
              list json)''')
 database.commit()
 guild = 635976654111506446
