a4487d7c7e97441c1e85fb5c0a045981ce814b3f
Dyslexus <96348686+Dragonpro9827@users.noreply.github.com>
2022-10-14 11:42:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -171,7 +171,8 @@ async def on_message(message):
                 new_data.append(i.split("**"))
             b = [[i for i in item if i != ''] for item in new_data]
             data = [item for item in b if item != []]
-            print(data[0][0])
+            if int((data[0][0]).replace("#","")) > 125:
+              return
             date2 = (str(message.created_at)[0:10])
             if "*TOP USERS*" == (message.embeds[0].title):
               try:
