e6c7f81117eb0c7dd482c7ef79d920cb5b6cab74
Dyslexus <96348686+Dragonpro9827@users.noreply.github.com>
2022-10-14 17:39:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,9 @@ async def find(ctx, name=None):
   for x in data:
     for i in data[x]:
       server = (i[1])
+      print(server)
       s = SequenceMatcher(None, (server[1:-2]), name)
-      if s.ratio() > 0.6:
+      if s.ratio() > 0.75:
         await ctx.channel.send(f"{(i[1])[:-1]} current count is {i[2]}, rank is {i[0]}")
         return
   await ctx.channel.send("Couldn't find the server you are looking for")
