6236e5311a10462aa52809f66955f5e8687c5441
Dyslexus <96348686+Dragonpro9827@users.noreply.github.com>
2022-10-14 17:45:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ async def find(ctx, name=None):
   for x in data:
     for i in data[x]:
       server = (i[1])
-      print(server)
       s = SequenceMatcher(None, (server[1:-2]), name)
+      print(f"{server[1:-2]} [{s.ratio()}] {name}")
       if s.ratio() > 0.75:
         await ctx.channel.send(f"{(i[1])[:-1]} current count is {i[2]}, rank is {i[0]}")
         return
