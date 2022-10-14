2a50c6a3947eb20b0e50a5ce7097040f816e2a09
Dyslexus <96348686+Dragonpro9827@users.noreply.github.com>
2022-10-14 17:19:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ async def find(ctx, name=None):
   except:
     pass
   name += ","
-  print(name)
   today = datetime.datetime.now()
   date = f"{today.year}-{today.month}-{today.day}"
   c.execute("select list from lb where time=%s", (date,))
@@ -51,8 +50,8 @@ async def find(ctx, name=None):
   for x in data:
     for i in data[x]:
       server = (i[1])
-      print(server)
       print(server[:-2])
+      print(name)
       if str(server[:-2]) == str(name):
         await ctx.channel.send(f"{(i[1])[:-1]} current count is {i[2]}, rank is {i[0]}")
         return
