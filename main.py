d034e87289647cf726fdbe6f415036801107ecb4
Dyslexus <96348686+Dragonpro9827@users.noreply.github.com>
2022-10-14 10:15:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -80,9 +80,8 @@ async def ulb(ctx, date=None, page=1):
           else:
             for key in data_yest:
               for x in (data_yest[key]):
-                if x[1] == (data_yesterday[counter])[1]:
-                  print(x[1])
-                  print(i[1])
+                if x[1] == i[1]:
+                  print((data_yesterday[counter])[1])
                   print(int((x[2]).replace(",", "")))
                   print(int(((data_yesterday[counter])[2]).replace(",", "")))
                   today_count, yesterday_count = int((x[2]).replace(",", "")), int(((data_yesterday[counter])[2]).replace(",", ""))
