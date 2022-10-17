01e6cc565e7c40b377a0e504151263337a210567
Dyslexus <96348686+Dragonpro9827@users.noreply.github.com>
2022-10-17 21:00:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def daily(ctx, mode="lb"):
   for i in data_dict:
     send+=f"**{(i[0])[:-1]}** {i[1]}\n"
 		counter+=1
-    if counter == 10:
+    if counter >= 10:
 			return
   embed=discord.Embed(title=f"Top 10 Daily {mode}", description=send, color=0xf1f3e1)
   embed.set_footer(text="-help | May vary depending on time logged\n\n")
